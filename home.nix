913556465e0eedccd8f4106482744dc6d7a4dd61
Sean Gilligan <sean@msgilligan.com>
2025-01-26 20:51:45 -0800

    home.nix: Use pkgs.gitMinimal
    Hopefully this will speed things up and avoid downloading Python and Perl.

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   # Git config will typically be placed in ~/.config/git/config
   programs.git = {
     enable = true;
+    package = pkgs.gitMinimal;  # Minimal Git without Perl or Python
     aliases = {
       ci = "commit";
       co = "checkout";
