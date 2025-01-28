84d01c78964ddb7dfa50471f03b8658870f604c8
Sean Gilligan <sean@msgilligan.com>
2025-01-27 18:28:25 -0800

    Reflect nixos-lima move to nixos-lima/nixos-lima

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    limainit.url = "github:msgilligan/nixos-lima/msgilligan-main";
+    limainit.url = "github:nixos-lima/nixos-lima/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
