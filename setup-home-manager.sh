66843d5f000e94344fd51ebe40831c1616b20251
Sean Gilligan <sean@msgilligan.com>
2025-02-11 13:31:41 -0800

    improve argument handling in setup scripts
    * Require hostname
    * Parameterize config-repo and config-name
    * Display help when no arguments are provided
    * Use better variable names
    * Update GitHub CI workflow
    * Update README

--- setup-home-manager.sh
+++ setup-home-manager.sh
@@ -2,28 +2,55 @@
 #
 # This script sets up Home Manager in newly created Lima-NixOS guest by checking out
 # a Home Manager configuration from a Git repo and initializing Home Manager.
-#
-# If parameter $1 is specified it will be used as the main username within the instance,
-# otherwise it will be set to $USER.
-#
 set -eux
 
-LIMA_NAME=nixsample
-LIMA_USER=${1:-$USER}
-LIMA_HOME=/home/${LIMA_USER}.linux
-CONFIG_DIR=$LIMA_HOME/.config
+# Git Repo containing a flake.nix containing Home Manager configurations
+# In the sample the repo is the same as the NixOS configuration repo, but they can be different
+DEFAULT_CONFIG_REPO='https://github.com/nixos-lima/nixos-lima-config-sample.git'
 
-HOME_CONFIG_REPO=https://github.com/nixos-lima/nixos-lima-config-sample.git
+display_help() {
+    echo
+    echo "Usage: $0 guest-hostname [guest-user] [config-repo]"
+    echo "Defaults are:"
+    echo "guest-user: $USER"
+    echo "config-repo: $DEFAULT_CONFIG_REPO"
+    echo
+}
+
+# Check if no arguments are provided
+set +x
+if [ $# -eq 0 ]; then
+    display_help
+    exit 1
+fi
+set -x
+
+# Name of Lima VM (Guest Host), required parameter
+GUEST_HOST_NAME=${1}
+
+# Name of main user in Guest OS, defaults to logged-in USER
+GUEST_USER=${2:-$USER}
+GUEST_HOME=/home/${GUEST_USER}.linux
+GUEST_CONFIG_DIR=${GUEST_HOME}/.config
+
+# Home Manager configuration to use, if not provided use default
+GUEST_CONFIG_REPO=${4:-$DEFAULT_CONFIG_REPO}
+
+set +x
+echo
+echo Configuring \"${GUEST_USER}@${GUEST_HOST_NAME}\" using \""$GUEST_CONFIG_REPO#$GUEST_USER"\"
+echo
+set -x
 
 # Create ~/.config if it doesn't already exist
-limactl shell $LIMA_NAME -- mkdir -p $CONFIG_DIR
+limactl shell $GUEST_HOST_NAME -- mkdir -p $GUEST_CONFIG_DIR
 
-# Checkout HOME_CONFIG_REPO containing your Home Manager configuration flake
-limactl shell $LIMA_NAME -- git clone $HOME_CONFIG_REPO $CONFIG_DIR/home-manager
+# Checkout $GUEST_CONFIG_REPO containing your Home Manager configuration flake
+limactl shell $GUEST_HOST_NAME -- git clone $GUEST_CONFIG_REPO $GUEST_CONFIG_DIR/home-manager
 
 # Configure subuid/subgid support for running rootless Podman services
-#limactl shell $LIMA_NAME -- sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $LIMA_USER
+#limactl shell $GUEST_HOST_NAME -- sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $LIMA_USER
 
 # Initialize Home Manager
-limactl shell $LIMA_NAME -- nix run home-manager/master -- init --switch
+limactl shell $GUEST_HOST_NAME -- nix run home-manager/master -- init --switch
 
--- setup-rebuild-nixos.sh
+++ setup-rebuild-nixos.sh
@@ -1,32 +1,63 @@
 #!/bin/bash
-#
 # This script sets up a newly created Lima-NixOS guest by checking out
-# both a Home Manager configuration and a NixOS system configuration from Git
-# and initializing Home Manager and rebuilding NixOS.
-#
+# both a NixOS system configuration from Git and rebuilding NixOS.
 set -eux
 
-LIMA_NAME=nixsample
-LIMA_USER=${1:-$USER}
-LIMA_HOME=/home/${LIMA_USER}.linux
-CONFIG_DIR=$LIMA_HOME/.config
+# Name of NixOS default configuration to apply (from the repo's flake.nix)
+DEFAULT_CONFIG_NAME='sample'
+# Git Repo containing a flake.nix containing NixOS configurations
+# In the sample the repo is the same as Home Manager repo, but they can be different
+DEFAULT_CONFIG_REPO='https://github.com/nixos-lima/nixos-lima-config-sample.git'
 
-# In the sample this is the same as Home Manager repo, but they can be different
-NIXOS_CONFIG_REPO=https://github.com/nixos-lima/nixos-lima-config-sample.git
+display_help() {
+    echo
+    echo "Usage: $0 guest-hostname [guest-user] [config-name] [config-repo]"
+    echo "Defaults are:"
+    echo "guest-user: $USER"
+    echo "config-name: $DEFAULT_CONFIG_NAME"
+    echo "config-repo: $DEFAULT_CONFIG_REPO"
+    echo
+}
+
+# Check if no arguments are provided
+set +x
+if [ $# -eq 0 ]; then
+    display_help
+    exit 1
+fi
+set -x
+
+# Name of Lima VM (Guest Host), required parameter
+GUEST_HOST_NAME=${1}
+
+# Name of main user in Guest OS, defaults to logged-in USER
+GUEST_USER=${2:-$USER}
+GUEST_HOME=/home/${GUEST_USER}.linux
+GUEST_CONFIG_DIR=${GUEST_HOME}/.config
+
+GUEST_CONFIG_NAME=${3:-$DEFAULT_CONFIG_NAME}
+# NixOS configuration to use, if not provided use default
+GUEST_CONFIG_REPO=${4:-$DEFAULT_CONFIG_REPO}
+
+set +x
+echo
+echo Configuring \"$GUEST_HOST_NAME\" using \""$GUEST_CONFIG_REPO#$GUEST_CONFIG_NAME"\"  via user \"$GUEST_USER\"
+echo
+set -x
 
 # Create ~/.config if it doesn't already exist
-limactl shell $LIMA_NAME -- mkdir -p $CONFIG_DIR
+limactl shell $GUEST_HOST_NAME -- mkdir -p $GUEST_CONFIG_DIR
 
-# Checkout NIXOS_CONFIG_REPO containing your NixOS host configuration flake
-limactl shell $LIMA_NAME -- git clone --bare $NIXOS_CONFIG_REPO $CONFIG_DIR/nixos-config
+# Checkout $GUEST_CONFIG_REPO containing your NixOS host configuration flake
+limactl shell $GUEST_HOST_NAME -- git clone --bare $GUEST_CONFIG_REPO $GUEST_CONFIG_DIR/nixos-config
 
 # Setup NixOS system config in separate-git-dir owned by user, with working directory in /etc/nixos
-limactl shell $LIMA_NAME -- sudo git init --separate-git-dir=$CONFIG_DIR/nixos-config --shared=group /etc/nixos
-limactl shell $LIMA_NAME -- sudo chown $LIMA_USER:wheel -R $CONFIG_DIR/nixos-config
-limactl shell $LIMA_NAME -- sudo bash -c "cd /etc/nixos ; git checkout master"
-limactl shell $LIMA_NAME -- sudo nixos-rebuild boot --flake .#sample
-sleep 5
-limactl stop $LIMA_NAME
-limactl start $LIMA_NAME
+limactl shell $GUEST_HOST_NAME -- sudo git init --separate-git-dir=$GUEST_CONFIG_DIR/nixos-config --shared=group /etc/nixos
+limactl shell $GUEST_HOST_NAME -- sudo chown $GUEST_USER:wheel -R $GUEST_CONFIG_DIR/nixos-config
+limactl shell $GUEST_HOST_NAME -- sudo bash -c "cd /etc/nixos ; git checkout master"
+limactl shell $GUEST_HOST_NAME -- sudo nixos-rebuild boot --flake .#$GUEST_CONFIG_NAME
+sleep 1
+limactl stop $GUEST_HOST_NAME
+limactl start $GUEST_HOST_NAME
 
 
