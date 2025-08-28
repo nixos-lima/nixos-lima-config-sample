74c4bd1fa66b4e8878b3dfeb3c77f6ae9ef3e690
Sean Gilligan <sean@msgilligan.com>
2025-08-28 14:32:46 -0700

    flake.nix: nixos-lima input follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-lima.url = "github:nixos-lima/nixos-lima/master";
+    nixos-lima = {
+      url = "github:nixos-lima/nixos-lima/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
